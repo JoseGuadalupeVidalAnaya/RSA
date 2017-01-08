6ac3c78f8657aabcd2c1011633b74b2491d1c7dc
JoseGuadalupeVidalAnaya <joseguadalupevidalanaya@gmail.com>
2017-01-08 11:38:02 -0600

    Funcionando

--- src/pp/RSA.java
+++ src/pp/RSA.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
  */
 public class RSA
 {
-    private BigInteger p, q, n,r;
+    private BigInteger p, q, n, r;
     private BigInteger e, d;
 
     boolean setPQ(String p, String q)
@@ -35,46 +35,48 @@ public class RSA
         n = p.multiply(q);
         r = p.subtract(BigInteger.valueOf(1));
         r = r.multiply(q.subtract(BigInteger.valueOf(1)));
-        e=n.subtract(new BigInteger("1"));
+        e = n.subtract(new BigInteger("1"));
         while (!e.isProbablePrime(10))
-            e=e.subtract(new BigInteger("1"));
+            e = e.subtract(new BigInteger("1"));
         d = e.modInverse(r);
     }
+
     String cifrar(String men)
     {
-        String s="";
-        byte b[]=men.getBytes();
-        BigInteger c[]=new BigInteger[b.length];
+        String s = "";
+        byte b[] = men.getBytes();
+        BigInteger c[] = new BigInteger[b.length];
 
         for (int i = 0; i < b.length; i++)
         {
-            c[i]=new BigInteger(""+b[i]);
+            c[i] = new BigInteger("" + b[i]);
             System.out.println(c[i]);
-            s=s+c[i].modPow(e,n)+"\t";
+            s = s + c[i].modPow(e, n) + "\t";
         }
         return s;
     }
+
     String decifrar(String men)
     {
-        String s="";
-        StringTokenizer st=new StringTokenizer(men);
-        BigInteger b[]=new BigInteger[st.countTokens()];
-        for (int i = 0; i <b.length ; i++)
+        String s = "";
+        StringTokenizer st = new StringTokenizer(men);
+        BigInteger b[] = new BigInteger[st.countTokens()];
+        for (int i = 0; i < b.length; i++)
         {
-            b[i]=new BigInteger(st.nextToken());
-            s=s+(char)b[i].modPow(d,n).intValue();
+            b[i] = new BigInteger(st.nextToken());
+            s = s + (char) b[i].modPow(d, n).intValue();
         }
         return s;
     }
 
     public String getE()
     {
-        return "e: "+e;
+        return "e: " + e;
     }
 
     public String getD()
     {
-        return "d: "+d;
+        return "d: " + d;
     }
 
     public String getN()
